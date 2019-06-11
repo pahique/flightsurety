d58cdf3125638a9936ba1e1131eb7e9257c69276
Paulo H. C. Oliveros <paulohco@gmail.com>
2019-06-11 00:44:49 -0300

    chore: missing file in the list

--- webpack.config.dapp.js
+++ webpack.config.dapp.js
@@ -12,6 +12,7 @@ module.exports = {
     new CopyWebpackPlugin([{ from: "./src/dapp/index.html", to: "index.html" }]),
     new CopyWebpackPlugin([{ from: "./src/dapp/styles.css", to: "styles.css" }]),
     new CopyWebpackPlugin([{ from: "./src/dapp/flight.jpg", to: "flight.jpg" }]),
+    new CopyWebpackPlugin([{ from: "./src/dapp/config.json", to: "config.json" }]),
   ],
   devServer: { 
     contentBase: path.join(__dirname, "dist"), 
